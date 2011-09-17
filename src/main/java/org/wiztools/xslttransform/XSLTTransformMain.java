6a0223587a3916ebbaa62dfb7398efc1402f47aa
Subhash Chandran <subwiz@gmail.com>
2011-09-17 03:40:35 +0000

    help added.

--- src/main/java/org/wiztools/xslttransform/XSLTTransformMain.java
+++ src/main/java/org/wiztools/xslttransform/XSLTTransformMain.java
@@ -18,22 +18,30 @@ public class XSLTTransformMain {
     private static final int EXIT_CODE_TRANSFORMER_EXCEPTION = 2;
     
     private static void printHelp(PrintStream out) {
-        out.println("Usage: java -jar xslt-transform.jar stylesheet file [file ...]");
+        out.println("Usage: java -jar xslt-transform-VERSION.jar stylesheet file [file ...]");
     }
     
     public static void main(String[] arg) {
-        if(arg.length < 2) {
+        if(arg.length == 1 && (arg[0].equals("-h") || arg[0].equals("--help"))) {
+            printHelp(System.out);
+            return;
+        }
+        else if(arg.length < 2) {
             printHelp(System.err);
             System.exit(EXIT_CODE_INVALID_PARAM);
         }
         
+        // When there are 2 or more arguments present:
         try{
+            // Argument 1 is the stylesheet:
             File stylesheet = new File(arg[0]);
 
             TransformerFactory transFactory = TransformerFactory.newInstance();
             Transformer transformer = transFactory.newTransformer(
                     new StreamSource(stylesheet));
 
+            // All arguments other than the first are XML documents to be
+            // transformed:
             boolean firstTimeInLoop = true;
             for(String dataFileStr: arg) {
                 if(firstTimeInLoop) {
